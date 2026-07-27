9a01e1c482213d2c09655bdc6fba17995813aeb7
davep <davep@granet.tech>
2026-07-27 19:17:24 -0400

    From Dave: Codex Site Updates

---
+++ assets/js/tawk-chat.js
@@ -0,0 +1,14 @@
+window.Tawk_API = window.Tawk_API || {};
+window.Tawk_LoadStart = new Date();
+
+(function loadTawkChat() {
+  const script = document.createElement('script');
+  const firstScript = document.getElementsByTagName('script')[0];
+
+  script.async = true;
+  script.src = 'https://embed.tawk.to/6a53d615f8f56a1d4cf66a2d/1jtbnoh3g';
+  script.charset = 'UTF-8';
+  script.setAttribute('crossorigin', '*');
+
+  firstScript.parentNode.insertBefore(script, firstScript);
+})();
