0e80f6ccf47b159b60e75cef5f9ece33a99a9e87
samuraiblood2 <nicholas.citarelli@granet.tech>
2026-08-15 11:54:55 -0400

    Fix Mercury public intake origin

--- assets/js/contact-config.js
+++ assets/js/contact-config.js
@@ -1,4 +1,4 @@
 window.GRANET_CONTACT_CONFIG = Object.freeze({
   // Public, non-secret Mercury Public Website Intake endpoint.
-  endpoint: 'https://internal.granet.tech/api/intake/public/b8ccd1c6fa896bc06190aad990611b54/'
+  endpoint: 'https://mercury.granet.tech/api/intake/public/b8ccd1c6fa896bc06190aad990611b54/'
 });
